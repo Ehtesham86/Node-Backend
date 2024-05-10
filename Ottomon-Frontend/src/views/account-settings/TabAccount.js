faf42a9f1636b21b6d5c964351a3a25350eaf4fe
Ehteshambutt <buttehtesham86@gmail.com>
2024-05-11 03:41:44 +0500

    Add my code files

---
+++ Ottomon-Frontend/src/views/account-settings/TabAccount.js
@@ -0,0 +1,71 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import Alert from '@mui/material/Alert'
+import Select from '@mui/material/Select'
+import { styled } from '@mui/material/styles'
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import InputLabel from '@mui/material/InputLabel'
+import AlertTitle from '@mui/material/AlertTitle'
+import IconButton from '@mui/material/IconButton'
+import CardContent from '@mui/material/CardContent'
+import FormControl from '@mui/material/FormControl'
+import Button from '@mui/material/Button'
+
+// ** Icons Imports
+import Close from 'mdi-material-ui/Close'
+
+const ImgStyled = styled('img')(({ theme }) => ({
+  width: 120,
+  height: 120,
+  marginRight: theme.spacing(6.25),
+  borderRadius: theme.shape.borderRadius
+}))
+
+const ButtonStyled = styled(Button)(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    textAlign: 'center'
+  }
+}))
+
+const ResetButtonStyled = styled(Button)(({ theme }) => ({
+  marginLeft: theme.spacing(4.5),
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    marginLeft: 0,
+    textAlign: 'center',
+    marginTop: theme.spacing(4)
+  }
+}))
+
+const TabAccount = () => {
+  // ** State
+  const [openAlert, setOpenAlert] = useState(true)
+  const [imgSrc, setImgSrc] = useState('/images/avatars/1.png')
+
+  const onChange = file => {
+    const reader = new FileReader()
+    const { files } = file.target
+    if (files && files.length !== 0) {
+      reader.onload = () => setImgSrc(reader.result)
+      reader.readAsDataURL(files[0])
+    }
+  }
+
+  return (
+    <CardContent>
+      <form>
+      I am categories page
+      </form>
+    </CardContent>
+  )
+}
+
+export default TabAccount
